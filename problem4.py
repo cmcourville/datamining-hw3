51d9ab6ce2fe254bc84f82ffe0155362142be910
cmcourville <32441018+cmcourville@users.noreply.github.com>
2019-11-14 09:50:03 -0500

    problem 4

--- problem4.py
+++ problem4.py
@@ -30,7 +30,9 @@ def pairwise_user_sim(R):
     '''
     #########################################
     ## INSERT YOUR CODE HERE
+
     S = pairwise_item_sim(R.T)
+    
     #########################################
     return S 
 
@@ -58,6 +60,7 @@ def predict(R, S, i, j):
     '''
     #########################################
     ## INSERT YOUR CODE HERE
+    
     p = p3.predict(R.T, S.T, j, i) 
 
     #########################################
